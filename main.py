6123bc67792dabe14bafa193f21e88c2aa677ddc
There Is No TIme <37583483+thereisnotime@users.noreply.github.com>
2023-04-29 03:17:29 +0300

    Adding validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import re
 from pptx import Presentation
 from pptx.util import Inches
 from pptx.enum.text import PP_ALIGN
@@ -6,13 +7,44 @@ from pptx.enum.text import PP_ALIGN
 input_file = "input.json"
 output_file = "output.pptx"
 
+validation_max_content_words = 45
+validation_max_bullet_points = 7
+validation_max_title_length = 50
+
 def load_slide_data(filename):
     with open(filename, "r") as f:
         data = json.load(f)
     return data
 
+def validate_slide_data(data):
+    slides = data["slides"]
+    
+    # Rule 1: The data must start and end with a title slide
+    if slides[0]["type"] != "title" or slides[-1]["type"] != "title":
+        raise ValueError("Presentation must start and end with a title slide")
+
+    # Rule 2: A single slide's text must not exceed 44 words
+    for slide in slides:
+        if slide["type"] == "text":
+            content = slide["content"]
+            word_count = len(re.findall(r'\w+', content))
+            if word_count > validation_max_content_words:
+                raise ValueError(f"Slide '{slide['title']}' has more than {validation_max_content_words} words")
+
+    # Rule 3: More rules that will guarantee good-looking presentations
+    for slide in slides:
+        if slide["type"] == "bullet_points":
+            if len(slide["content"]) > validation_max_bullet_points:
+                raise ValueError(f"Slide '{slide['title']}' has more than {validation_max_bullet_points} bullet points")
+
+    # Rule 4: Slide titles must not exceed 50 characters
+    for slide in slides:
+        if len(slide['title']) > validation_max_title_length:
+            raise ValueError(f"Slide title '{title}' is longer than {validation_max_title_length} characters")
+
+
 def create_title_slide(presentation, title):
-    slide_layout = presentation.slide_layouts[5]
+    slide_layout = presentation.slide_layouts[0]
     slide = presentation.slides.add_slide(slide_layout)
     title_shape = slide.shapes.title
     title_shape.text = title
@@ -47,6 +79,7 @@ def create_image_slide(presentation, title, image_path):
 
 if __name__ == "__main__":
     data = load_slide_data(input_file)
+    validate_slide_data(data)
     slides = data["slides"]
 
     presentation = Presentation()
